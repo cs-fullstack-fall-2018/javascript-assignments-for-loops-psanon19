2c1fd82499b1a3754122f999f6027fd7a941b28b
Pelane Sanon <p_sanon@aol.com>
2018-08-02 09:35:42 -0500

    Help

--- Fizz-Buzz.js
+++ Fizz-Buzz.js
@@ -9,17 +9,17 @@ for(numberCount=0;numberCount<enterNumber;numberCount++)
     if (numberCount%3===0 && numberCount%5===0)
     {
         console.log((numberCount) + "\n");
-        print("FIZZBUZZ", numberCount)
+        console.log("FIZZBUZZ", numberCount)
     }
     else if (numberCount%3===0)
     {
         console.log((numberCount) + "\n");
-        print("FIZZ", numberCount)
+        console.log("FIZZ", numberCount)
     }
     else if (numberCount%5==0)
     {
         console.log((numberCount) + "\n")
-        print("BUZZ", numberCount)
+        console.log("BUZZ", numberCount)
     }
     else
     {
